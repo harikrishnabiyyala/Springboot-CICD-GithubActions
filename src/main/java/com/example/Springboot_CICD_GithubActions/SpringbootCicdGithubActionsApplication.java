a2027e04618bddb096efdeaf7775bce0814298fa
Hari Krishna <harikrishnabiyyala@gmail.com>
2024-11-04 11:12:35 +0530

    init

---
+++ src/main/java/com/example/Springboot_CICD_GithubActions/SpringbootCicdGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_CICD_GithubActions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Springboot_CICD_GithubActions/SpringbootCicdGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_CICD_GithubActions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
